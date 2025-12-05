675d872ab87d5891eb34d818e6ec2b1553c94a21
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-12-05 09:19:01 -0300

    Refactor: tratativa de erro e criação do caminho absoluto da pasta de arquivos

--- config.py
+++ config.py
@@ -1,15 +1,24 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# 1. Define o caminho absoluto da pasta onde este arquivo está
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
+    # 3. Tenta pegar a URL
+    uri = os.getenv("DATABASE_URL")
 
-    uri = os.getenv('DATABASE_URL')
+    # Trava de segurança: Se não achar, para tudo e avisa você
+    if not uri:
+        raise RuntimeError("ERRO CRÍTICO: A variável 'DATABASE_URL' não foi encontrada. "
+                           "Verifique se o arquivo .env existe na raiz do projeto e se a variável está escrita corretamente.")
 
-    if uri and uri.startswith("postgres://"):
+
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.getenv("SECRET_KEY", "chave-padrao-insegura")
