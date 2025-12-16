c371e57e648b68214cdf1ed4e46604f555856d2e
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-12-16 12:28:02 -0300

    refactor: consertando os imports

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,32 +1,30 @@
 from flask import Blueprint, request, jsonify
-from app.extensions import db
+from app.extensions import db, jwt
 from app.models import User
 from flask_jwt_extended import create_access_token
 
+# Define o Blueprint
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json() or {}
-
     email = data.get('email')
     password = data.get('password')
 
     if not email or not password:
-        return jsonify({"error": "Email e Senha são obrigatórios"}), 400
+        return jsonify({'error': 'Email e senha obrigatorios'}), 400
 
+    # Busca usuário
     user = db.session.execute(
         db.select(User).where(User.email == email)
     ).scalar_one_or_none()
 
-    # Verifica se o usuário existe e a senha está correta
+    # Verifica senha
     if not user or not user.check_password(password):
-        return jsonify({"error": "Credenciais inválidas"}), 401
+        return jsonify({'error': 'Credenciais invalidas'}), 401
 
-    # Cria o token de acesso JWT
-    access_token = create_access_token(identity=str(user.id), additional_claims={"tenant_id": user.tenant_id})
+    # Gera token
+    access_token = create_access_token(identity=str(user.id))
 
-    return jsonify({
-        'message': "Login realizado com sucesso!",
-        'access_token': access_token,
-        'user': {'id': user.id, 'name': user.name, 'email': user.email,}
-    }), 200
+    return jsonify(access_token=access_token), 200
